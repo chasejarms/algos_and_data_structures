aef660120233d5ae8567384175a152a401bf6d33
Chase Armstrong <chasejarms@gmail.com>
2017-04-24 11:34:54 -0700

    Add in find operation and begin union

--- union_find.js
+++ union_find.js
@@ -1,20 +1,16 @@
 // let sz = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 // let nodes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// union
-
-// isConnected
-
 class QuickUnion {
   constructor(n) {
     this.size = this.createSizeArray(n);
-    this.nodes = this.createNodesArray(n);
+    this.parent = this.createNodesArray(n);
   }
 
   createSizeArray(n) {
     let sizeArray = []
     for (let i = 0; i < n; i ++) {
-      sizeArray.push(0);
+      sizeArray.push(1);
     }
     return sizeArray;
   }
@@ -22,24 +18,46 @@ class QuickUnion {
   createNodesArray(n) {
     let nodesArray = [];
     for (let i = 0; i < n; i++) {
-      nodesArray.push(0);
+      nodesArray.push(i);
     }
     return nodesArray;
   }
 
   union(node1, node2) {
+    let rootOne = this._root(node1);
+    let rootTwo = this._root(node2);
 
+    if (rootOne === rootTwo) {
+      return 'already unified';
+    }
+
+    if (this.size[rootOne] < this.size[rootTwo]) {
+      
+    } else {
+
+    }
   }
 
   find(node1, node2) {
+    return this._root(node1) === this._root(node2);
+  }
+
+  isConnected () {
 
   }
 
   _root(n) {
-
+    if (n === this.parent[n]) {
+      return n;
+    }
+    return this._root(this.parent[n]);
   }
 
   _connectGrandparent() {
 
   }
+
 }
+
+let quickUnion = new QuickUnion(10);
+console.log(quickUnion);
