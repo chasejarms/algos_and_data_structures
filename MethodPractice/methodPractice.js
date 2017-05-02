4a1930fdf2bc6131bf789b5171f6bdb0417e3b33
Chase Armstrong <chasejarms@gmail.com>
2017-05-02 15:54:32 -0700

    Change testing structure

--- MethodPractice/methodPractice.js
+++
@@ -1,7 +0,0 @@
-// splice
-
-// input: ['apple', 'banana', 'orange']
-// output: ['apple', 'pear', 'grapefruit', 'banana', 'orange']
-
-const spliceAdd = ['apple', 'banana', 'orange'].splice(1, 0, 'pear', 'grapefruit');
-// const spliceAdd = ['apple', 'banana', 'orange'].splice(1, 0, 'pear', 'grapefruit');
--- MethodPractice/methodPractice.test.js
+++ MethodPractice/methodPractice.test.js
@@ -1,7 +1,41 @@
-const spliceAdd = require('./methodPractice.js');
-
 describe('splice', () => {
+
+  let fruits = ['apple', 'banana', 'orange'];
+  fruits.splice(1,0, 'pear', 'grapefruit');
+
   it ('adds elements to the middle of the array and deletes nothing', () => {
-    expect(spliceAdd).toEqual(['apple', 'pear', 'grapefruit', 'banana', 'orange']);
+    expect(fruits).toEqual(['apple', 'pear', 'grapefruit', 'banana', 'orange']);
+  });
+
+  let cities = ['San Francisco', 'Salt Lake', 'Chicago', 'Miami', 'Los Angeles'];
+  cities.splice(2, 2);
+
+  it ('delete items from the array', () => {
+    expect(cities).toEqual(['San Francisco', 'Salt Lake', 'Los Angeles']);
+  });
+});
+
+describe('indexOf', () => {
+  const arr = ['hello', 'what up', 'goodbye', 'what up', 'goodbye'];
+  indexOfHello = arr.indexOf('hello');
+
+  it ('finds the index', () => {
+    expect(indexOfHello).toBe(0);
+  });
+
+  indexOfGoodbye = arr.indexOf('goodbye');
+
+  it ('find the first index', () => {
+    expect(indexOfGoodbye).toBe(2);
+  });
+});
+
+describe('lastIndexOf', () => {
+
+  const arr = ['hello', 'hello'];
+  const lastHello = arr.lastIndexOf('hello');
+
+  it ('finds the last index', () => {
+    expect(lastHello).toBe(1);
   });
 });
