ab2519f10ad0a29c8301ace1aeb427b2fded9b9f
Chase Armstrong <chasejarms@gmail.com>
2017-05-09 10:10:57 -0700

    Set up request elevator and commit two and a half hour code

--- Elevators/elevatorController.js
+++ Elevators/elevatorController.js
@@ -4,15 +4,22 @@ const Elevator = require('./Elevator');
 
 class ElevatorController {
   constructor(numElevators, numFloors) {
+    this.totalElevators = numElevators;
+    this.unoccupiedElevatorCount = numElevators;
+    this.occupiedElevatorCount = 0;
     this.unoccupiedElevators = this._createFloors(numFloors);
     this.occupiedElevators = this._createFloors(numFloors);
-    this.maintenanceElevator = [];
+    this.maintenanceElevator = new LinkedList();
 
     this._setElevators(numElevators);
   }
 
   requestElevator(floor) {
-    
+    // first look for unoccupied elevators on that floor
+
+    // then see if anything is moving in that direction and is occupied
+
+    // then find the closest unoccupied elevator
   }
 
   _createFloors(numFloors) {
